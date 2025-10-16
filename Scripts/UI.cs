88127e87d1b36bb0051635d340a4586ae7f917e6
Ates Balci <atesbalci@gmail.com>
2025-10-16 13:52:01 +0300

    Implemented the intro splash

--- Scripts/UI.cs
+++ Scripts/UI.cs
@@ -1,12 +1,16 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Godot;
+using Range = Godot.Range;
 
 public partial class UI : Node
 {
     private GameData _gameData;
     private Range _bloodMeter;
     private Range _annoyanceMeter;
-    private PackedScene _gameOverPanel;
     private bool _gameOver;
+    private Splash _splash;
     
     public void Initialize(GameData gameData)
     {
@@ -17,17 +21,38 @@ public partial class UI : Node
     {
         _bloodMeter = GetNode<Range>(GetMeta("BloodMeter").AsNodePath());
         _annoyanceMeter = GetNode<Range>(GetMeta("AnnoyanceMeter").AsNodePath());
-        _gameOverPanel = GetMeta("GameOver").As<PackedScene>();
+        SpawnSplash(SplashImage.Start);
     }
 
     public override void _Process(double delta)
     {
         _bloodMeter.SetValue(_gameData.MosquitoSize);
         _annoyanceMeter.SetValue(_gameData.Annoyance);
-        if (_gameData.IsGameOver && !_gameOver)
+        if (_gameData.IsGameOver)
         {
-            _gameOver = true;
-            AddChild(_gameOverPanel.Instantiate());
+            if (!_gameOver)
+            {
+                _gameOver = true;
+                SpawnSplash(SplashImage.End);
+                AddChild(GetMeta("GameOver").As<PackedScene>().Instantiate());
+            }
         }
+        else if (Input.IsKeyLabelPressed(Key.Space))
+        {
+            DespawnSplash();
+        }
+    }
+
+    private void SpawnSplash(SplashImage splashImage)
+    {
+        _splash = (Splash) GetMeta("Splash").As<PackedScene>().Instantiate();
+        _splash.Show(splashImage);
+        AddChild(_splash);
+    }
+
+    private void DespawnSplash()
+    {
+        if (_splash == null) return;
+        RemoveChild(_splash);
     }
 }
